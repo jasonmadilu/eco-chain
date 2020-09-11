6af36beca3a8167c6c4b148e46b67ab098eb4c71
jasonmadilu <70958043+jasonmadilu@users.noreply.github.com>
2020-09-11 09:43:21 +0200

    deploy test 2

---
+++ build/precache-manifest.b216df72d922c16740e9c74a9cfffd11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345dfef5b946031e3b40e599caf078fc",
+    "url": "/eco-chain/index.html"
+  },
+  {
+    "revision": "d9606f5e6c5652650b64",
+    "url": "/eco-chain/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "13decb29cfd962a00915",
+    "url": "/eco-chain/static/css/main.258ab78d.chunk.css"
+  },
+  {
+    "revision": "d9606f5e6c5652650b64",
+    "url": "/eco-chain/static/js/2.b065767a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eco-chain/static/js/2.b065767a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13decb29cfd962a00915",
+    "url": "/eco-chain/static/js/main.848f5d25.chunk.js"
+  },
+  {
+    "revision": "bf80c265a7233e947664",
+    "url": "/eco-chain/static/js/runtime-main.685938c8.js"
+  },
+  {
+    "revision": "d5ff1945241b0976e3cf03a84d4444dd",
+    "url": "/eco-chain/static/media/318x180.d5ff1945.svg"
+  }
+]);
